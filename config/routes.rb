b34787336793e29987c30c1de709a0e4b5151485
Alberto Alcaraz <jaalcarazz@yahoo.com>
2012-02-23 20:02:44 -0600

    Haml gem installed, created pages model and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 TraineeApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  
+  root :to => "pages#index"
+
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
